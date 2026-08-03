0ec0e7a97f4a8e3c691ade7eb5ff111f5b947c01
Muhammad Nurhabibi <nurhabibi666@gmail.com>
2026-08-03 18:25:11 +0700

    Commit : Install Tamplate KaiAdmin & merapikan + Memperbaiki Halaman Login & Logout

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ["resources/sass/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // Menyembunyikan deprecation warning dari dependensi node_modules (seperti Bootstrap)
+                quietDeps: true,
+                silenceDeprecations: [
+                    "color-functions",
+                    "import",
+                    "global-builtin",
+                ],
+            },
+        },
+    },
 });

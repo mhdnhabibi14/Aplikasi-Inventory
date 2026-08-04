32c5a0d2e9aa7da5171d3febdb08231372f1ee2d
Muhammad Nurhabibi <nurhabibi666@gmail.com>
2026-08-05 02:58:12 +0700

    Commit : Membuat CRUD Hal Kategori Produk, Lengkap dengan fitur PerPage, filter dan reset filter

--- app/Http/Controllers/KategoriProdukController.php
+++ app/Http/Controllers/KategoriProdukController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\storeKategoriProdukRequest;
+use App\Http\Requests\updateKategoriProdukRequest;
 use App\Models\KategoriProduk;
 use Illuminate\Http\Request;
+
+use function Laravel\Prompts\confirm;
 
 class KategoriProdukController extends Controller
 {
@@ -11,8 +15,40 @@ class KategoriProdukController extends Controller
     public function index()
     {
         $pageTitle = $this->pageTitle;
+        $perPage = request()->query('perPage') ?? 10;
+        $search = request()->query('search');
         $query = KategoriProduk::query();
-        $kategori = $query->paginate(10);
+
+        if ($search) {
+            $query->where('nama_kategori', 'like', '%' . $search . '%');
+        }
+
+        $kategori = $query->paginate($perPage)->appends(request()->query());
+        confirmDelete('Hapus data Kategori Produk tidak dapat dibatalkan, Lanjutkan ?');
         return view('kategori-produk.index', compact('pageTitle', 'kategori'));
+    }
+
+    public function store(storeKategoriProdukRequest $request)
+    {
+        KategoriProduk::create([
+            'nama_kategori' => $request->nama_kategori
+        ]);
+        toast()->success('Kategori Produk berhasil ditambahkan');
+        return redirect()->route('master-data.kategori-produk.index');
+    }
+
+    public function update(updateKategoriProdukRequest $request, KategoriProduk $kategoriProduk)
+    {
+        $kategoriProduk->nama_kategori = $request->nama_kategori;
+        $kategoriProduk->save();
+        toast()->success('Kategori Produk berhasil diupdate');
+        return redirect()->route('master-data.kategori-produk.index');
+    }
+
+    public function destroy(KategoriProduk $kategoriProduk)
+    {
+        $kategoriProduk->delete();
+        toast()->success('Kategori Produk berhasil dihapus');
+        return redirect()->route('master-data.kategori-produk.index');
     }
 }
--- resources/views/kategori-produk/index.blade.php
+++ resources/views/kategori-produk/index.blade.php
@@ -3,10 +3,25 @@
 @section('content')
     <div class="card">
         <div class="card-body py-5">
-            <div>
-                <x-kategori-produk.form-kategori-produk />
+            <div class="row">
+                {{-- filter --}}
+                <div class="row col-10 align-items-center justify-content-between">
+                    <div class="col-1">
+                        <x-per-page-option />
+                    </div>
+                    <div class="col-9">
+                        <x-filter-by-field term="search" placeholder="Cari kategori produk" />
+                    </div>
+                    <div class="col-1">
+                        <x-button-reset-filter route="master-data.kategori-produk.index" />
+                    </div>
+                </div>
+                {{-- end filter --}}
+                <div class="col-2 d-flex justify-content-ent">
+                    <x-kategori-produk.form-kategori-produk />
+                </div>
             </div>
-            <table class="table">
+            <table class="table mt-5">
                 <thead>
                     <tr>
                         <th class="text-center" style="width: 15px">No</th>
@@ -19,7 +34,13 @@
                         <tr>
                             <td>{{ $index + 1 }}</td>
                             <td>{{ $item->nama_kategori }}</td>
-                            <td></td>
+                            <td>
+                                <div class="d-flex align-items-center gap-1">
+                                    <x-kategori-produk.form-kategori-produk id="{{ $item->id }}" />
+                                    <x-confirm-delete id="{{ $item->id }}"
+                                        route="master-data.kategori-produk.destroy" />
+                                </div>
+                            </td>
                         </tr>
                     @empty
                         <tr>
@@ -28,6 +49,7 @@
                     @endforelse
                 </tbody>
             </table>
+            {{ $kategori->links() }}
         </div>
     </div>
 @endsection

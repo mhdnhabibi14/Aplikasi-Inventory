9cad203c89b381029ac5a227d997d878346ac22d
Muhammad Nurhabibi <nurhabibi666@gmail.com>
2026-08-08 02:12:04 +0700

    Commit : Membuat Halaman Stok Barang

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\KategoriProdukController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\StokBarangController;
 use App\Http\Controllers\VarianProdukController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -22,5 +23,6 @@ Route::middleware('auth')->group(function () {
         Route::resource('kategori-produk', KategoriProdukController::class);
         Route::resource('produk', ProdukController::class);
         Route::resource('varian-produk', VarianProdukController::class)->only(['store', 'update', 'destroy']);
+        Route::resource('stok-barang', StokBarangController::class)->only('index');
     });
 });
